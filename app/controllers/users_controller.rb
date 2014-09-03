155fd408ec9f381f53ddbbfd1a7d5ed59fdb9bac
Yusuke Nozoe <yusuke.nozoe@dena.jp>
2014-09-03 09:23:22 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -44,7 +43,10 @@ class UsersController < ApplicationController
    # Before filters
 
    def signed_in_user
-     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+     unless signed_in?
+       store_location
+       redirect_to signin_url, notice: "Please sign in." 
+     end
    end
 
    def correct_user
