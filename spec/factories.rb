a9812d359cfb55229398f51b1ccfec77d774a75a
Yusuke Nozoe <yusuke.nozoe@dena.jp>
2014-09-03 15:53:27 +0900

    micropost refinements

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :user do
-    #name "Michael Hartl"
-    #email "michael@example.com"
     sequence(:name) { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
@@ -10,5 +8,10 @@ FactoryGirl.define do
     factory :admin do
       admin true
     end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 end
