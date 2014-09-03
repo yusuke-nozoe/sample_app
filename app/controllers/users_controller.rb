c99e295a7a677a17461a048878a034cb5bf904ac
Yusuke Nozoe <yusuke.nozoe@dena.jp>
2014-09-03 09:08:45 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -36,5 +38,11 @@ class UsersController < ApplicationController
 
    def user_params
      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   end
+
+   # Before filters
+
+   def signed_in_user
+     redirect_to signin_url, notice: "Please sign in." unless signed_in?
    end
 end
